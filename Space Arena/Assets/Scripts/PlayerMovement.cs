98bff302c890c4a46fa01ce11552dfcee9517dda
omerkarj <omerkarj@gmail.com>
2015-11-30 20:57:49 +0200

    facing mouse and movement fixed

--- Space Arena/Assets/Scripts/PlayerMovement.cs
+++ Space Arena/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,21 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-    public float speed; 
-    public float turnSpeed;
-    public float speedLimit;
-    public float turnLimit;
+    public float speed = 1000; 
+    public float speedLimit = 6;
+    public float turnSpeed = 200;
+    public float turnSpeedLimit = 4;
+    public Transform model;
+
+    private const float MIN_Y_ROTATION = 110;
+    private const float MAX_Y_ROTATION = 250;
 
     private Rigidbody rb;
     private Vector3 mousePosition;
     private float rotation;
 
+    enum Facing { left, right };
+    private Facing facing = Facing.right;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +25,10 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
     void Update() {
+        FaceMousePosition();
 
+        // Keep transform Y-Axis and X-Axis rotations at 0
+        transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z);
     }
  
     void FixedUpdate() {
@@ -32,26 +41,48 @@ public class PlayerMovement : MonoBehaviour {
             SpeedBurst(new Vector3(0, speed, 0), turnSpeed);
         if (Input.GetKeyDown(KeyCode.DownArrow))
             SpeedBurst(new Vector3(0, -speed, 0), -turnSpeed);
+
         // Keep velocity under check
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, speedLimit);
-
-        //Grab the current mouse position on the screen
-        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - Camera.main.transform.position.z));
-        rotation = Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg - 90;
-        rb.transform.eulerAngles = new Vector3(transform.eulerAngles.x, rotation, transform.eulerAngles.z);
-        Debug.Log(rb.transform);
-        Debug.Log(transform);
 	}
 
+    // Adds force to the player in the appropriate direction
     void SpeedBurst(Vector3 input, float torque) {
         Vector3 move = input * Time.deltaTime;
 
-        if (rb.angularVelocity.z > turnLimit)
+        if (rb.angularVelocity.z > turnSpeedLimit)
             torque = 0;
 
         if (move != Vector3.zero) {
             rb.AddForce(move);
             rb.AddRelativeTorque(new Vector3(0, 0, torque * Time.deltaTime));
+        }
+    }
+
+    // Keeps the player facing the mouse cursor at all times
+    void FaceMousePosition()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - Camera.main.transform.position.z));
+        Vector3 xDistance = mousePosition - transform.position;
+        float relativeMousePosition = Vector3.Dot(xDistance, transform.right.normalized);
+
+        // Decide which way to face the player according to mouse position
+        if (relativeMousePosition <= 0) {
+            Debug.Log("facing left");
+            facing = Facing.left;
+        }
+        else if (relativeMousePosition > 0) {
+            Debug.Log("facing right");
+            facing = Facing.right;
+        }
+
+        // Ease the player rotation to face the mouse
+        float currentYRotation = model.localEulerAngles.y;
+        if (facing == Facing.right && currentYRotation > MIN_Y_ROTATION) {
+            model.localEulerAngles = Vector3.Slerp(model.localEulerAngles, new Vector3(0, MIN_Y_ROTATION, 0), Time.deltaTime * 1.6f);    
+        }
+        else if (facing == Facing.left && currentYRotation < MAX_Y_ROTATION) {
+            model.localEulerAngles = Vector3.Slerp(model.localEulerAngles, new Vector3(0, MAX_Y_ROTATION, 0), Time.deltaTime * 1.6f);    
         }
     }
 }
