638ab93c2196fb36158994436f5f4e932927969e
omer182 <omer182@gmail.com>
2015-12-11 14:06:14 +0200

    Add HUD features

--- Space Arena/Assets/Scripts/PlayerController.cs
+++ Space Arena/Assets/Scripts/PlayerController.cs
@@ -3,22 +3,18 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-    public int hp;
-    private Text hpText;
+    
     private WeaponController weaponController;
-
+    public int hp;
 	// Use this for initialization
 	void Start () {
-        hp = 100;
-        hpText = GameObject.FindGameObjectWithTag("HPText").GetComponent<Text>();
         weaponController = GameObject.FindGameObjectWithTag("Weapon").GetComponent<WeaponController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        // update HP in HUD
-        hpText.text = "HP: " + hp;
+        
+       
 	}
 
     void OnTriggerEnter (Collider other) {
@@ -31,19 +27,6 @@ public class PlayerController : MonoBehaviour {
                 break;
             case "WeaponDrop-AlienWeapon":
                 weaponController.SwitchWeapon(WeaponController.Weapons.alienWeapon);
-                break;
-            // Health pickup
-            case "HealthDrop":
-                hp += 10;
-                break;
-            // Hit by enemy shot
-            case "EnemyShot":
-                hp -= 10;
-                break;
-            // Collision with enemy / asteroid
-            case "EnemyKamikaze":
-            case "Asteroid":
-                hp -= 20;
                 break;
         }
         Destroy(other.gameObject);
