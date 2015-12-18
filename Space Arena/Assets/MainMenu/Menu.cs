6f3896a4748f6289532a12fde154eac3cb3fb17f
omer182 <omer182@gmail.com>
2015-12-18 17:03:04 +0200

    added changes

---
+++ Space Arena/Assets/MainMenu/Menu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    private Animator _animator;
+    private CanvasGroup _canvasGroup;
+
+    public bool IsOpen
+    {
+        get { return _animator.GetBool("IsOpen"); }
+        set { _animator.SetBool("IsOpen", value); }
+    }
+
+    public void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+
+        var rect = GetComponent<RectTransform>();
+        rect.position = new Vector2(0, 0);
+        _canvasGroup.interactable = true;
+        
+
+    }
+
+    public void Update()
+    {
+
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("MainMenuOpen"))
+        {
+            _canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+        }
+        else
+        {
+            _canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+        }
+
+    }
+
+}
