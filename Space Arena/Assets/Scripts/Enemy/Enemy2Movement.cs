888a3dc09b9978ec545cbe3ca65604804de30275
MeGustian <yoro187@gmail.com>
2015-12-19 21:47:15 +0200

    enemy 2 progress

--- Space Arena/Assets/Scripts/Enemy/Enemy2Movement.cs
+++ Space Arena/Assets/Scripts/Enemy/Enemy2Movement.cs
@@ -1,50 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
 
-public class EnemyMovement : MonoBehaviour {
+public class Enemy2Movement : MonoBehaviour {
 
     // Push parameters
     public float MinForce = 40f;
     public float MaxForce = 100f;
     public float DirectionChangeInterval = 1f;
-    public int healthCounter = 3;
-    
+    public int healthCounter = 6;
+    public GameObject Child;
 
 
+    private bool edgeDown = false;
+    private bool edgeUp = false;
     private float directionChangeInterval;
     private float x;
     private float y;
 
     // Use this for initialization
-    void Start ()
-    {
-        int pathNo = (int)UnityEngine.Random.Range(1f, 7f);
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("EnemyPath" + pathNo), "time", 10));
-        directionChangeInterval = DirectionChangeInterval;
-        Push();
-	}
+    void Start () {
+        directionChangeInterval = DirectionChangeInterval;    
+        StartCoroutine(couroutineThatWaits());
+        StartCoroutine(movementCoroutine());
+    }
 
-    // Update is called once per frame
-    void Update ()
+    IEnumerator movementCoroutine()
+    {
+        while (true)
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Enemy2Path"), "time", 5));
+            yield return new WaitForSeconds(5f);
+        }
+
+    }
+    IEnumerator couroutineThatWaits()
+    {
+        yield return new WaitForSeconds(3f);
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(Child, transform.position + new Vector3(-1, -1, 0), new Quaternion());
+            yield return new WaitForSeconds(3f);
+        }
+        
+        
+
+    }
+        
+	
+	// Update is called once per frame
+	void Update ()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        
+
         //var dir = player.transform.position - transform.position;
         //transform.rotation = Quaternion.FromToRotation(transform.pos, dir);
-        
+
         if (player != null)
         {
             //transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
             transform.LookAt(player.transform.position);
         }
 
-        directionChangeInterval -= Time.deltaTime;
-        if (directionChangeInterval < 0)
-        {
-            Push();
-            directionChangeInterval = DirectionChangeInterval;
-        }
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp(pos.x, 0.1f, 0.9f);
         pos.y = Mathf.Clamp(pos.y, 0.1f, 0.9f);
@@ -63,7 +79,7 @@ public class EnemyMovement : MonoBehaviour {
 
     }
 
-    void OnTriggerEnter (Collider other)
+    void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided!!");
         if (other.gameObject.tag == "PlayerShot")
@@ -71,6 +87,7 @@ public class EnemyMovement : MonoBehaviour {
             healthCounter--;
         }
 
+       
         // Check if enemy is dead
         if (healthCounter == 0 || other.gameObject.tag == "Missile")
         {
---
+++ Space Arena/Assets/Scripts/Enemy/Enemy2SmallMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Enemy2SmallMovement : MonoBehaviour {
+
+    
+    public float distance = 1f;
+    public float speed = 2f;
+
+
+
+	// Use this for initialization
+	void Start () {
+        iTween.PunchPosition(gameObject, new Vector3(3, 0, 0), 5f);
+        StartCoroutine(couroutineThatWaits());
+        
+	}
+
+    private IEnumerator couroutineThatWaits()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        yield return new WaitForSeconds(5f);
+        iTween.MoveTo(gameObject, player.transform.position, 2f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.1f, 0.9f);
+        pos.y = Mathf.Clamp(pos.y, 0.1f, 0.9f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
+
+ 
