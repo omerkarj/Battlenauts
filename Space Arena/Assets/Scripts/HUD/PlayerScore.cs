b2c39717360ac73c79618a470010380e98bf93c9
Roy Simha <roysimha@Roys-MacBook-Pro-2.local>
2015-12-20 16:09:41 +0200

    Special up is bounded by public variable

--- Space Arena/Assets/Scripts/HUD/PlayerScore.cs
+++ Space Arena/Assets/Scripts/HUD/PlayerScore.cs
@@ -6,10 +6,13 @@ public class PlayerScore : MonoBehaviour {
 
     // Use this for initialization
     public int Score;
+	private int powerUpCounter;
     public Text scoreText;
+	public int pointsForPowerUp=1000;
 	void Start () {
         Score = 0;
         updateScore(0);
+		powerUpCounter = 0;
  
 	}
 	
@@ -20,6 +23,14 @@ public class PlayerScore : MonoBehaviour {
     {
         Score += addValue;
         scoreText.text = "Score: " + Score;
-
+		AddpowerUp (addValue);
     }
+	private void AddpowerUp(int addValue){
+		powerUpCounter += addValue;
+		if (powerUpCounter >= pointsForPowerUp) {
+			powerUpCounter=0;
+			Debug.Log("power up is ready");
+			gameObject.GetComponent<PlayerController>().isPowerupOn=true;
+		}
+	}
 }
