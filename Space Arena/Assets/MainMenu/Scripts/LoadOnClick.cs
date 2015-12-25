df4d618a6a4ae955e0b5c88d1f028a51cb1edc86
MeGustian <yoro187@gmail.com>
2015-12-26 01:52:26 +0200

    second enemy

--- Space Arena/Assets/MainMenu/Scripts/LoadOnClick.cs
+++ Space Arena/Assets/MainMenu/Scripts/LoadOnClick.cs
@@ -5,6 +5,7 @@ public class LoadOnClick : MonoBehaviour {
 
 public void LoadScene(int level)
     {
+        PlayerPrefs.SetInt("lastLevel", Application.loadedLevel);
         Application.LoadLevel(level);
     }
 }
--- Space Arena/Assets/Scripts/Enemy/Enemy2SmallMovement.cs
+++ Space Arena/Assets/Scripts/Enemy/Enemy2SmallMovement.cs
@@ -9,18 +9,22 @@ public class Enemy2SmallMovement : MonoBehaviour
     public int killReward = 20;
     public Transform explosionParticles;
 
-
+    private float forceInterval = 0.5f;
+    private float forceTime = 3.5f;
 
     // Use this for initialization
     void Start()
     {
-        iTween.PunchPosition(gameObject, new Vector3(Random.Range(1f, 15f), 0, 0), 5f);
+        
+        
         StartCoroutine(couroutineThatWaits());
+        
 
     }
 
     private IEnumerator couroutineThatWaits()
     {
+        iTween.PunchPosition(gameObject, new Vector3(0, 0, Random.Range(1f, 15f)), 5f);
         var player = GameObject.FindGameObjectWithTag("Player");
         yield return new WaitForSeconds(5f);
         iTween.MoveTo(gameObject, player.transform.position, 2f);
@@ -29,6 +33,24 @@ public class Enemy2SmallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+        forceInterval -= Time.deltaTime;
+        forceTime -= Time.deltaTime;
+
+        if (forceInterval == 0)
+        {
+            forceInterval = 0.5f;
+            Vector3 direction = new Vector3(Random.Range(1f, 15f), 0, 0) - transform.position;
+            gameObject.GetComponent<Rigidbody>().AddForce(direction * 5, ForceMode.Force);
+        }
+
+        if (forceTime == 0)
+        {
+            forceTime = 3.5f;
+
+        }
+        */
+
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp(pos.x, 0.1f, 0.9f);
         pos.y = Mathf.Clamp(pos.y, 0.1f, 0.9f);
